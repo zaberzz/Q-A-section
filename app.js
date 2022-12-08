cc3b8ab02c617adfb9ff3183aab6dbc09df1e054
Saba <saba.sadegh67@gmail.com>
2022-12-08 13:54:17 +0330

    add functionallity to btns ,using selector

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // traversing the DOM
-const closeBtns = document.querySelectorAll(".question-btn");
+// const closeBtns = document.querySelectorAll(".question-btn");
 
-closeBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    e.currentTarget.parentElement.parentElement.classList.toggle("show-text");
+// closeBtns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     e.currentTarget.parentElement.parentElement.classList.toggle("show-text");
+//   });
+// });
+
+//using selector inside an element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
   });
 });
